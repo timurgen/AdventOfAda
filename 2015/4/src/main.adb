cc160a113c748da13b9a431e6bbaf3153df84bb9
Timur Samkharadze <timur.samkharadze@sysco.no>
2018-10-27 13:32:12 +0200

    2015/4 part 1

---
+++ 2015/4/src/main.adb
@@ -0,0 +1,14 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with GNAT.MD5;    use GNAT.MD5;
+with Ada.Strings.Fixed;
+procedure Main is
+   Input : String := "bgvyzdsv";
+begin
+   for I in 0 .. Integer'Last loop
+      if Digest (Input & Ada.Strings.Fixed.Trim (Integer'Image (I), Ada.Strings.Left)) (1 .. 5) = "00000" then
+         Put_Line (I'Img);
+         exit;
+      end if;
+
+   end loop;
+end Main;
