9a0fdbbde8ddf1be758ee78e4dac49b378f67a21
Timur Samkharadze <timur.samkharadze@sysco.no>
2018-10-29 12:04:03 +0100

    2015/5 part 1

---
+++ 2015/5/src/main.adb
@@ -0,0 +1,87 @@
+with Ada.Text_IO;       use Ada.Text_IO;
+with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+procedure Main is
+
+   function Count_Vowels (S : String) return Integer is
+      Result       : Integer := 0;
+      Vowels       : String  := "aeiou";
+      Current_Char : Character;
+   begin
+      for I in 1 .. S'Length loop
+         Current_Char := S (I);
+         if Index (Vowels, "" & Current_Char) > 0 then
+            Result := Result + 1;
+         end if;
+
+      end loop;
+      return Result;
+   end Count_Vowels;
+
+   function Is_String_Nice_V1 (S : String) return Boolean is
+      Has_At_Least_3_Vowels : Boolean := False;
+      Has_Repeated_Chars    : Boolean := False;
+   begin
+
+      for I in S'Range loop
+         if I < S'Length and then S (I) = S (I + 1) then
+            Has_Repeated_Chars := True;
+            exit;
+         end if;
+      end loop;
+
+      if Count_Vowels (S) >= 3 then
+         Has_At_Least_3_Vowels := True;
+      end if;
+
+      return Has_At_Least_3_Vowels and Has_Repeated_Chars;
+   end Is_String_Nice_V1;
+
+   function Is_String_Naughty_V1 (S : String) return Boolean is
+      Ab : String := "ab";
+      Bc : String := "cd";
+      Pq : String := "pq";
+      Xy : String := "xy";
+
+   begin
+      if Index (S, Ab) > 0 or else Index (S, Bc) > 0 or else Index (S, Pq) > 0 or else Index (S, Xy) > 0 then
+         return True;
+      end if;
+
+      if Count_Vowels (S) < 3 then
+         return True;
+      end if;
+
+      return False;
+   end Is_String_Naughty_V1;
+
+   Input_File : File_Type;
+   Is_Nice    : Integer := 0;
+   Is_Naughty : Integer := 0;
+
+   Something_Weird : exception;
+begin
+   Open (File => Input_File, Mode => In_File, Name => "input.txt");
+
+   loop
+      declare
+         Line : String := Get_Line (Input_File);
+      begin
+         if not Is_String_Naughty_V1 (Line) and then Is_String_Nice_V1 (Line) then
+            Is_Nice := Is_Nice + 1;
+         end if;
+         if Is_String_Naughty_V1 (Line) or else not Is_String_Nice_V1 (Line) then
+            Is_Naughty := Is_Naughty + 1;
+         end if;
+
+      end;
+
+   end loop;
+exception
+   when End_Error =>
+      if Is_Open (Input_File) then
+         Put_Line ("Nice: " & Is_Nice'Img);
+         Put_Line ("Naughty: " & Is_Naughty'Img);
+         Close (Input_File);
+      end if;
+
+end Main;
