86f8dccab58f7411a2fa18830de6dbced4d4da5f
= <timur.samkharadze@gmail.com>
2018-12-01 13:53:36 +0100

    2018/01 part 2

--- 2018/1/src/main.adb
+++ 2018/1/src/main.adb
@@ -25,26 +25,67 @@ procedure Main is
       return Integer'Value (Number_Str);
    end Parse_Value;
 
+   --part 2 vars
+   Already_Seen_Number : Integer := Start_Frequence;
+
+   type Already_Seen_Index_Arr is
+     array (Integer range -1_000_000 .. 100_000_000) of Boolean;
+   type Already_Seen_Index_Arr_Access is access Already_Seen_Index_Arr;
+
+   Already_Seen_Arr : Already_Seen_Index_Arr_Access := new Already_Seen_Index_Arr'(others => False);
+
+   function Is_Already_Seen (I : Integer) return Boolean is
+   begin
+      return Already_Seen_Arr (I);
+   end Is_Already_Seen;
+
+   procedure Add_To_Already_Seen (I : Integer) is
+   begin
+      Already_Seen_Arr (I) := True;
+   end Add_To_Already_Seen;
+
+   procedure Register_Already_Seen_But_Only_First_Time (I : Integer) is
+   begin
+      if Already_Seen_Number /= 0 then
+         Already_Seen_Number := I;
+      end if;
+
+   end Register_Already_Seen_But_Only_First_Time;
+
 begin
-   Text_IO.Open
-     (File => Input_File,
-      Mode => Text_IO.In_File,
-      Name => "input.txt");
    loop
-      exit when Text_IO.End_Of_File (Input_File);
-      declare
-         Line      : String      := Text_IO.Get_Line (Input_File);
-         Operation : Operation_T := Parse_Operation (Line);
-         Num       : Integer     := Parse_Value (Line);
-      begin
-         if Operation = ADD then
-            Start_Frequence := Start_Frequence + Num;
-         elsif Operation = SUBSTRACT then
-            Start_Frequence := Start_Frequence - Num;
-         else
-            raise Unknon_Operation;
-         end if;
-      end;
+      exit when Already_Seen_Number /= 0;
+      Text_IO.Open
+        (File => Input_File,
+         Mode => Text_IO.In_File,
+         Name => "input.txt");
+      loop
+         exit when Text_IO.End_Of_File (Input_File);
+         declare
+            Line      : String      := Text_IO.Get_Line (Input_File);
+            Operation : Operation_T := Parse_Operation (Line);
+            Num       : Integer     := Parse_Value (Line);
+         begin
+            if Operation = ADD then
+               Start_Frequence := Start_Frequence + Num;
+            elsif Operation = SUBSTRACT then
+               Start_Frequence := Start_Frequence - Num;
+            else
+               raise Unknon_Operation;
+            end if;
+            -- part 2
+            if not Is_Already_Seen (Start_Frequence) then
+               Add_To_Already_Seen (Start_Frequence);
+            else
+               Already_Seen_Number := Start_Frequence;
+               exit;
+            end if;
+
+         end;
+      end loop;
+      Text_IO.Close(Input_File);
+      Text_IO.Put_Line (Integer'Image (Start_Frequence));
+      Text_IO.Put_Line (Integer'Image (Already_Seen_Number));
    end loop;
-   Text_IO.Put_Line (Integer'Image (Start_Frequence));
+
 end Main;
